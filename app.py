6a8ff798b8397996a28b257596ef22ace5601d64
inespmbb-cyber <inespmbb@gmail.com>
2026-02-09 12:40:52 +0000

    Segunda alteração
    Criar ano nascimento

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-idade = input("Introduza a sua idade: ")
-if idade.isnumeric():
-    idade = int(idade)
+from datetime import date
+
+def calcular_idade(ano_nascimento: int) -> int:
+    ano_atual: int = date.today().year
+    return ano_atual - ano_nascimento
+
+ano_nascimento_input = input("Introduza o seu ano de nascimento: ")
+
+if ano_nascimento_input.isnumeric():
+    ano_nascimento_arg = int(ano_nascimento_input)
+    idade = calcular_idade(ano_nascimento_arg)
 
     if idade >= 18:
         print("Maior de idade")
